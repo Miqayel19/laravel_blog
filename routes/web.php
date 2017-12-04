963fc577185a869548d33c42cfc6849bec2d09e2
Michael Avetisyan <michaelavetisyan92@gmail.com>
2017-12-04 11:38:42 +0400

    updated

--- routes/web.php
+++ routes/web.php
@@ -12,26 +12,16 @@
 */
 
 Route::get('/', function () {
-	
+    
     return view('welcome');
 });
 
 Auth::routes();
 
 Route::get('/home', 'HomeController@index');
-// Route::get('/posts/', 'PostsController@index');
-// Route::get('/posts/create', 'PostsController@create');
-// Route::post('/posts/store', 'PostsController@store');
-// Route::delete('/posts/{id}','PostsController@destroy');
-// Route::get('/posts/{id}/edit', 'PostsController@edit');
-// Route::put('/posts/{id}','PostsController@update');
-// Route::get('/categories/', 'CategoriesController@index');
-// Route::get('/categories/create', 'CategoriesController@create');
-// Route::post('/categories/store', 'CategoriesController@store');
-// Route::delete('/categories/{id}','CategoriesController@destroy');
-// Route::get('/categories/{id}/edit','CategoriesController@edit');
-// Route::put('/categories/{id}','CategoriesController@update');
+
 Route::resource('categories', 'CategoriesController');
 Route::resource('posts', 'PostsController');
+
 Route::get('login/{provider}', 'Auth\RegisterController@redirectToProvider');
 Route::get('login/{provider}/callback', 'Auth\RegisterController@handleProviderCallback');
